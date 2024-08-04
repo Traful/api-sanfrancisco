a0013db7ac0d46ea8bcb99e5c703379ff3946acf
federicojaime <federiconj@gmail.com>
2024-08-04 02:15:56 -0300

    cambio en la ruta estadisticas para que de el total recaudado

--- objects/estadisticas.php
+++ objects/estadisticas.php
@@ -1,33 +1,38 @@
 <?php
-	//namespace objects;
+//namespace objects;
 
-	//use objects\Base;
-	require_once("objects/base.php");
+//use objects\Base;
+require_once("objects/base.php");
 
-	class Estadisticas extends Base {
-		private $table_name = "inscripciones";
+class Estadisticas extends Base
+{
+	private $table_name = "inscripciones";
 
-		// constructor
-		public function __construct($db) {
-			parent::__construct($db);
-		}
-		public function getEstadisticasTotalInscriptos() {
-			// Total de inscriptos
-			$query = "SELECT COUNT(*) AS cantidad FROM $this->table_name WHERE pagado = 1";
-			parent::getOne($query);
-			return $this;
-		}
+	// constructor
+	public function __construct($db)
+	{
+		parent::__construct($db);
+	}
+	public function getEstadisticasTotalInscriptos()
+	{
+		// Total de inscriptos
+		$query = "SELECT COUNT(*) AS cantidad, SUM(importe) AS total FROM $this->table_name WHERE pagado = 1";
+		parent::getOne($query);
+		return $this;
+	}
 
-		public function getEstadisticasByGeneros() {
-			// Cantidades por genero
-			$query = "SELECT genero, COUNT(*) AS cantidad FROM $this->table_name WHERE pagado = 1 GROUP BY genero";
-			parent::getAll($query);
-			return $this;
-		}
+	public function getEstadisticasByGeneros()
+	{
+		// Cantidades por genero
+		$query = "SELECT genero, COUNT(*) AS cantidad FROM $this->table_name WHERE pagado = 1 GROUP BY genero";
+		parent::getAll($query);
+		return $this;
+	}
 
-		public function getEstadisticasByItems() {
-			// Items (tipo de carrera/producto que se vende)
-			$query = <<<EOD
+	public function getEstadisticasByItems()
+	{
+		// Items (tipo de carrera/producto que se vende)
+		$query = <<<EOD
 			SELECT
 				items.titulo,
 				C1.cantidad,
@@ -45,22 +50,23 @@
 			INNER JOIN items ON
 				items.id = C1.idItem
 			EOD;
-			parent::getAll($query);
-			return $this;
-		}
-
-		public function getEstadisticasByEdades() {
-			// Categorías/Edad
-			$query = "SELECT categoria_edad, COUNT(*) AS cantidad FROM $this->table_name  WHERE pagado = 1 GROUP BY categoria_edad";
-			parent::getAll($query);
-			return $this;
-		}
-
-		public function getEstadisticasByTalles() {
-			// Talles de Remera
-			$query = "SELECT talle_remera, COUNT(*) AS cantidad FROM $this->table_name  WHERE pagado = 1 GROUP BY talle_remera";
-			parent::getAll($query);
-			return $this;
-		}
+		parent::getAll($query);
+		return $this;
 	}
-?>
+
+	public function getEstadisticasByEdades()
+	{
+		// Categorías/Edad
+		$query = "SELECT categoria_edad, COUNT(*) AS cantidad FROM $this->table_name  WHERE pagado = 1 GROUP BY categoria_edad";
+		parent::getAll($query);
+		return $this;
+	}
+
+	public function getEstadisticasByTalles()
+	{
+		// Talles de Remera
+		$query = "SELECT talle_remera, COUNT(*) AS cantidad FROM $this->table_name  WHERE pagado = 1 GROUP BY talle_remera";
+		parent::getAll($query);
+		return $this;
+	}
+}
